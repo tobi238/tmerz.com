39cf124d7285e422210fbe5eb5ffcb67f824a545
tobi238 <tobi238@gmail.com>
2019-02-03 14:04:31 +0100

    chore: fixed tslint errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +6,5 @@ import { HomeComponent } from './home/home.component';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Tobias Merz';  
+  title = 'Tobias Merz';
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 import { map, tileLayer } from 'leaflet';
 import { environment } from '../../environments/environment';
 
@@ -12,48 +12,52 @@ FontAwesome.library.add(faEnvelope, faXing, faLinkedinIn, faGithub);
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   public map;
   private mapState: any = {
-    center: [49.0076052,8.4026419],
+    center: [49.0076052, 8.4026419],
     zoom: 14,
     options: {
       zoomControl: false,
       dragging: false,
       doubleClickZoom: false,
       boxZoom: false,
-      scrollWheelZoom: false,
+      scrollWheelZoom: false
     }
-  }
+  };
 
-  public showCard: boolean = false
-  public showSocials: boolean = false
+  public showCard = false;
+  public showSocials = false;
 
-  private showPageLoading: any
-  private showAppAndCard: any
-  private showSocialsTimeoutId: any
+  private showPageLoading: any;
+  private showAppAndCard: any;
+  private showSocialsTimeoutId: any;
 
-  public start : any = {
+  public start: any = {
     loaded: false,
     loading: true
-  }
+  };
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
 
   ngAfterViewInit() {
     this.map = map('map', this.mapState.options).setView(this.mapState.center, this.mapState.zoom);
-    const layer = tileLayer('https://api.mapbox.com/styles/v1/tobi238/cj5mj7f4m3h0p2ss7v22uplf7/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      maxZoom: 18,
-      detectRetina: true,
-      accessToken: environment.MAPBOX_API_KEY
-    })
+    const layer = tileLayer(
+      'https://api.mapbox.com/styles/v1/tobi238/cj5mj7f4m3h0p2ss7v22uplf7/tiles/{z}/{x}/{y}?access_token={accessToken}',
+      {
+        attribution: `Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,
+        <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>`,
+        maxZoom: 18,
+        detectRetina: true,
+        accessToken: environment.MAPBOX_API_KEY
+      }
+    );
 
     // show card animation
     this.showAppAndCard = setTimeout(() => {
-      this.start.loading = false
+      this.start.loading = false;
       this.showCard = true;
     }, 300);
 
@@ -66,27 +70,23 @@ export class HomeComponent implements OnInit {
     layer.on('load', e => {
       // hide page_loading
       this.showPageLoading = setTimeout(() => {
-        this.start.loaded = true
+        this.start.loaded = true;
       }, 0);
-    })
+    });
     layer.addTo(this.map);
-    
   }
 
-  ngOnDestroy () {
-		if (this.showPageLoading) {
-			clearTimeout(this.showPageLoading);
+  ngOnDestroy() {
+    if (this.showPageLoading) {
+      clearTimeout(this.showPageLoading);
     }
-    
-		if (this.showAppAndCard) {
-			clearTimeout(this.showAppAndCard);
-		}
 
-		if (this.showSocialsTimeoutId) {
-			clearTimeout(this.showSocialsTimeoutId);
-		}
-	}
-  
+    if (this.showAppAndCard) {
+      clearTimeout(this.showAppAndCard);
+    }
 
-
+    if (this.showSocialsTimeoutId) {
+      clearTimeout(this.showSocialsTimeoutId);
+    }
+  }
 }
