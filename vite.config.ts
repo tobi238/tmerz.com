3153bb4afc4627802c4d384da51f519beff12d26
Tobias Merz <info@tmerz.com>
2026-01-11 18:18:38 +0100

    adjust vite base when deploying to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
     sourcemap: true,
   },
   server: {
     port: 3000,
     open: true,
   },
-})
+});
