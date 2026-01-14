49cf857ba13367eecf64fc5c63f9e2d180314a53
Tobias Merz <info@tmerz.com>
2026-01-14 21:45:34 +0100

    switch analytics to eu region

--- src/analytics.ts
+++ src/analytics.ts
@@ -8,8 +8,9 @@ const POSTHOG_API_KEY = import.meta.env.VITE_POSTHOG_API_KEY;
 
 if (POSTHOG_API_KEY) {
   posthog.init(POSTHOG_API_KEY, {
+    api_host: "https://eu.i.posthog.com",
+    defaults: "2025-11-30",
     cookieless_mode: "always",
-    person_profiles: "identified_only",
   });
 } else {
   // This will show up in dev if you haven't set up your .env file
